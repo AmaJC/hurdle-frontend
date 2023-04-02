0206413840f72f70e56a7e4c6ae604dedf3ef7ab
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2023-04-02 08:40:43 +0000

    Point Hurdle Frontend to official Hurdle Backend staff solution

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 // BACKEND_URL is the base URL to the Backend. You can optionally set this
-// variable with your own, e.g. "https://wordlebackend-1.amajc.repl.co".
+// variable with your own, e.g. "https://hurdle-backend.<YOUR_GITHUB_USER>.repl.co".
 // If this is set to "localstorage", then Hurdle will prompt the user.
-export const BACKEND_URL = 'https://tuffle-backend-solution.amajc.repl.co';
+export const BACKEND_URL = 'https://hurdle-backend-solution.amajc.repl.co';
